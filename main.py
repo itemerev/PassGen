0e63cec2105e9baac07a6a59bd946f902a1f970f
Ilya Temerev <91725919+itemerev@users.noreply.github.com>
2021-10-28 15:25:59 +0300

    Update main.py
    Выделил отдельную функцию main(), которая вызывается, если файл не является модулем.
    Создал отдельный класс Btn_action(QWidget) для Активных кнопок (ent, save, open, log), и перенес туда методы set() и click()(переназвал его generate()) из класса App(QWidget).
    Прописал метод generate(), который генерирует пароль из символов переменной self.symbols_for_generate и выводит его на экран label()

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from random import choice
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget
 
@@ -11,19 +12,36 @@ class App(QWidget):
         super().__init__()
         self.passgen = uic.loadUi('PassGen_Des01.ui')
         self.start()
-        self.set()
 
     def start(self):
         self.passgen.show()
 
-    def set(self):
-        self.passgen.btn_123.clicked.connect(lambda: self.click())
 
-    def click(self):
-        pass
+class Btn_action(QWidget):
+    def __init__(self):
+        self.num = 1
+        self.password = ''
+        self.symbols_for_generate = ''
+        self.digits = '0123456789'
+        super().__init__()
+        self.passgen = uic.loadUi('PassGen_Des01.ui')
+        self.set()
+        
+    def set(self):
+        self.passgen.btn_ent.clicked.connect(lambda: self.generate())
+
+    def generate(self):
+        for i in range(self.num):
+            self.password += choice(self.symbols_for_generate)
+        self.passgen.label.setText(self.password)
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = App()
+    btn_act = Btn_action()
+    app.exec_()
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    app.exec_()
+    main()
