bf1171eeb94fcc848e16da2dbc03fe04dc2c193e
itemerev <kidmoto3@gmail.com>
2021-10-29 20:11:32 +0300

    Пароль генерируется в зависимомсти от выбранных символов

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ from PyQt5.QtWidgets import QApplication, QWidget
 
 class App(QWidget):
     def __init__(self):
+        self.digits = '0123456789'
+        self.alpha_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        self.alpha_lower = 'abcdefghijklmnopqrstuvwxyz'
         self.num = 8
-        self.symbols_to_generate = '0123456789'
-        self.password = ''
         super().__init__()
         self.passgen = uic.loadUi('PassGen_Des01.ui')
         self.start()
@@ -24,7 +25,15 @@ class App(QWidget):
         self.passgen.btn_gen.clicked.connect(lambda: self.click())
 
     def click(self):
+        self.symbols_to_generate = ''
         self.password = ''
+        if self.passgen.btn_123.isChecked():
+            self.symbols_to_generate += self.digits
+        if self.passgen.btn_ABC.isChecked():
+            self.symbols_to_generate += self.alpha_upper
+        if self.passgen.btn_abc.isChecked():
+            self.symbols_to_generate += self.alpha_lower
+
         for i in range(self.num):
             self.password += choice(self.symbols_to_generate)
         self.passgen.label.setText(self.password)
@@ -34,6 +43,7 @@ def main():
     app = QApplication(sys.argv)
     ex = App()
     app.exec_()
+
 
 if __name__ == '__main__':
     main()
