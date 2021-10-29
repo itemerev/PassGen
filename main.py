2bbd6f5c5eb1281b7df4fa2927192d968170740f
itemerev <kidmoto3@gmail.com>
2021-10-29 20:01:38 +0300

    Создал функцию main() Создал действие кнопки GEN

--- main.py
+++ main.py
@@ -9,39 +9,31 @@ from PyQt5.QtWidgets import QApplication, QWidget
 
 class App(QWidget):
     def __init__(self):
+        self.num = 8
+        self.symbols_to_generate = '0123456789'
+        self.password = ''
         super().__init__()
         self.passgen = uic.loadUi('PassGen_Des01.ui')
         self.start()
+        self.set()
 
     def start(self):
         self.passgen.show()
 
-
-class Btn_action(QWidget):
-    def __init__(self):
-        self.num = 1
-        self.password = ''
-        self.symbols_for_generate = ''
-        self.digits = '0123456789'
-        super().__init__()
-        self.passgen = uic.loadUi('PassGen_Des01.ui')
-        self.set()
-        
     def set(self):
-        self.passgen.btn_ent.clicked.connect(lambda: self.generate())
+        self.passgen.btn_gen.clicked.connect(lambda: self.click())
 
-    def generate(self):
+    def click(self):
+        self.password = ''
         for i in range(self.num):
-            self.password += choice(self.symbols_for_generate)
+            self.password += choice(self.symbols_to_generate)
         self.passgen.label.setText(self.password)
 
 
 def main():
     app = QApplication(sys.argv)
     ex = App()
-    btn_act = Btn_action()
     app.exec_()
-
 
 if __name__ == '__main__':
     main()
