103a89426871913183216368e96d2a8fd6aeecc3
itemerev <kidmoto3@gmail.com>
2021-11-03 22:40:24 +0300

    Добавил действие кнопки ONE MORE для выбора длины генерируемого пароля, а также копирование пароля в буфер обмена.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import win32clipboard
 from random import choice
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget
@@ -15,29 +16,39 @@ class App(QWidget):
         self.alpha_lower = 'abcdefghijklmnopqrstuvwxyz'
         self.symbols = '~!@#$%^&*(()<>{}[]№?'
         self.special = 'IlO01'
-        self.num = 8
-        self.passgen = uic.loadUi('PassGen_Des01.ui')
+        self.num = 3
+        self.pass_gen = uic.loadUi('PassGen_Des01.ui')
         self.start()
         self.click()
 
     def start(self):
-        self.passgen.show()
+        self.pass_gen.show()
 
     def click(self):
-        self.passgen.btn_gen.clicked.connect(lambda: self.generate())
+        self.pass_gen.btn_gen.clicked.connect(lambda: self.generate())
+        self.pass_gen.btn_enter.clicked.connect(lambda: self.set_num())
+
+    def set_num(self):
+        self.num += 1
+        if self.num > 18:
+            self.num = 3
+        if self.num == 3 or self.num == 4:
+            self.pass_gen.label.setText(f'Длина {self.num} символа')
+        else:
+            self.pass_gen.label.setText(f'Длина {self.num} символов')
 
     def generate(self):
         symbols_to_generate = ''
         password = ''
-        if self.passgen.btn_123.isChecked():
+        if self.pass_gen.btn_123.isChecked():
             symbols_to_generate += self.digits
-        if self.passgen.btn_ABC.isChecked():
+        if self.pass_gen.btn_ABC.isChecked():
             symbols_to_generate += self.alpha_upper
-        if self.passgen.btn_abc.isChecked():
+        if self.pass_gen.btn_abc.isChecked():
             symbols_to_generate += self.alpha_lower
-        if self.passgen.btn_symbols.isChecked():
+        if self.pass_gen.btn_symbols.isChecked():
             symbols_to_generate += self.symbols
-        if not self.passgen.btn_special.isChecked():
+        if not self.pass_gen.btn_special.isChecked():
             temp_sym_to_gen = ''
             for c in symbols_to_generate:
                 if c not in self.special:
@@ -46,9 +57,13 @@ class App(QWidget):
         if symbols_to_generate:
             for i in range(self.num):
                 password += choice(symbols_to_generate)
-            self.passgen.label.setText(password)
+            self.pass_gen.label.setText(password)
+            win32clipboard.OpenClipboard()
+            win32clipboard.EmptyClipboard()
+            win32clipboard.SetClipboardText(password)
+            win32clipboard.CloseClipboard()
         else:
-            self.passgen.label.setText('ВЫБЕРИТЕ СИМВОЛЫ!')
+            self.pass_gen.label.setText('ВЫБЕРИТЕ СИМВОЛЫ!')
 
 
 def main():
